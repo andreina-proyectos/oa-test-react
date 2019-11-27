2922f40861aad3e06efe6272e11df97ec65d3a38
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-27 11:23:12 +0100

    Add list of cities from data on citiesList

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import dataCities from "./services/cities-china.json";
 import "./App.css";
-
-console.log(dataCities);
+import CitiesList from "./components/cities-list/CitiesList.js";
 
 function App() {
+  const [data, setData] = useState(dataCities.cities);
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="app-header">
         <h1 className="header__title">Cities of China</h1>
       </header>
+      <main className="app__main">
+        <CitiesList data={data} />
+      </main>
     </div>
   );
 }
--- src/components/cities-list/CitiesList.js
+++ src/components/cities-list/CitiesList.js
@@ -2,6 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./CitiesList.scss";
 
-const CitiesList = () => {};
+const CitiesList = props => {
+  const { data } = props;
+  return (
+    <div className="main__cities-list">
+      <ul className="cities-list">
+        {data.map(city => {
+          return (
+            <li key={city.id} className="cities-list__city">
+              <input type="checkbox" className="city__checkbox" />
+              <img src="" alt="" className="city__img" />
+              <p className="city__name">{city.name}</p>
+              <p className="city__">{city.chineseName}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default CitiesList;
