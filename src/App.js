16765165dffb6ec869538192ab6ed4ed2532e54f
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-27 11:03:19 +0100

    Add json data and create CitiesList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import dataCities from "./services/cities-china.json";
 import "./App.css";
+
+console.log(dataCities);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">lkjjpko cssww</header>
+      <header className="App-header">
+        <h1 className="header__title">Cities of China</h1>
+      </header>
     </div>
   );
 }
---
+++ src/components/cities-list/CitiesList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./CitiesList.scss";
+
+const CitiesList = () => {};
+
+export default CitiesList;
